63f12ae1ee3daa4416dafe95f62efa4a10c8b986
Fabio Rodrigues <fabiordrig@gmail.com>
2018-10-09 22:18:05 -0300

    adicionando readme

--- setup.py
+++ setup.py
@@ -10,21 +10,26 @@ def recebendoMsg(msg):
     nome = msg['chat']['first_name']
     print(nome+": "+msg['text'])
 
+    #Requisição da mensagem
+    #R = requests.get("curl 'localhost:5000/parse?q=tmensagem&project=teste' ")
+    #JSON do treinamento
+    #R.json()
+        #Requisicao do Rastro JS
+        #RastroJS = requests.get('http://localhost:3000/track/' + numeroEncomenda+'/json')
+        #Tratando o JSON que vem do Rastro JS
+        #RastroJS.json
+        #Dentro do json pegar o tipo "data" dentro dele o "track"e por fim trazer tudo o que tiver dentro
+        #Respondendo as informaçoes sobre a encomenda
+        #bot.sendMessage(chatID," ")
     if mensagem == '/start':
         bot.sendMessage(chatID,"Olá, " + nome +"!\nMeu nome é Lion, seu localizador de Encomendas!")
         bot.sendMessage(chatID,"Digite /encomenda mais o número da encomenda, para descobrir onde está sua encomenda\n")
-        bot.sendMessage(chatID,"Exemplo : /encomenda 74845784.")
+        bot.sendMessage(chatID,"Exemplo : /encomenda74845784.")
         mensagem = ''
     if  "/encomenda" in mensagem:
         numeroEncomenda=mensagem.replace('/encomenda','')
         mensagem = ''
-        #teste
-        print(numeroEncomenda)
-        #requisicao do Rastro JS
-        #r = requests.get('http://localhost:3000/track/' + numeroEncomenda+'/json')
-        #print(r.text)
-        #print(r)
-        #print(r.json)
+        bot.sendMessage(chatID,"Em Processo.")
     if mensagem != '':
         bot.sendMessage(chatID,"Tente usar /start para saber em que posso ajudá-lo.")
         
