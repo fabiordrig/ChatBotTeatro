489ca95c1377c3286b172c9c651369fbaaf4568d
Fabio Rodrigues <fabiordrig@gmail.com>
2018-10-24 23:00:11 -0300

    fix - comandos

--- commands.py
+++ commands.py
@@ -8,6 +8,6 @@ class Commands():
         elif msg["text"] == "/start":
             self.telegram.sendMessage(msg_id,"Olá para começar me diga qual tipo de evento você pretende ir")
         elif msg["text"] == "/sobre":
-            self.telegram.sendMessage(msg_id,"sou um bot criado na FATEC SJC, tenho dois pais e uma mãe, e eu estou aqui para ajudar você a se divertir")
+            self.telegram.sendMessage(msg_id,"sou um bot criado na FATEC SJC")
         else:
             self.telegram.sendMessage(msg_id,"Desculpe, não entedi o seu comando :/")
